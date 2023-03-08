fef0b47144c68f1024974dab8804d8d2ae38910a
Itallo de Sousa Lima <103972860+itallo894@users.noreply.github.com>
2023-03-08 11:25:28 -0300

    Update inicial.spec.js
    Adicionando um simples comentário.

--- cypress/integration/inicial.spec.js
+++ cypress/integration/inicial.spec.js
@@ -1,6 +1,6 @@
 it('Verificar se a aplicação web está online', function () {
     cy.visit('/')
-
+    //adicionando um comentario.
     cy.title()
         .should('eq', 'Samurai Barbershop by QAninja')
 
