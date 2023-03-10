d994a7c9ed8d48f658f2343126850f4696c65bbc
itallo SL <103972860+italloSL@users.noreply.github.com>
2023-03-10 15:47:52 -0300

    Criando task para torna meu teste independente.

--- cypress/integration/loginm2.spec.js
+++ cypress/integration/loginm2.spec.js
@@ -8,17 +8,35 @@ describe('login', function () {
         const user = {
             name: 'josé augusto',
             email: 'josé@samuraibs.com',
-            password: 'jose321'
+            password: 'jose321',
+            is_provider: true
 
         }
+
+        before(function () {
+            cy.task('removeUser', user.email)
+                .then(function (result) {
+                    console.log(result)
+                })
+
+            cy.request(
+                'POST',
+                'http://localhost:3333/users',
+                user
+            ).then(function (response) {
+                expect(response.status).to.eq(200)
+            })
+
+        })
 
         it('Acessando  a página', function () {
             loginPage.go()
             loginPage.form(user)
             loginPage.submit()
+
             dashPage.header.userlonggerIN(user.name)
 
-           
+
 
 
         })
